c5f26670ea67ea4948fec15416fcafce876abbeb
pranay-panchal <pranayy0208@gmail.com>
2019-05-26 12:55:07 +0530

    added button ng model text area two way binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+Component,
+OnInit
+} from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+selector: 'app-servers',
+templateUrl: './servers.component.html',
+styleUrls: ['./servers.component.scss']
 })
 export class ServersComponent implements OnInit {
+    allowNewServer = false;
+    serverCreationStatus = 'No Servers Created';
+    serverName = 'Test Server';
+constructor() {
+    setTimeout(() => {
+        this.allowNewServer = true;
+    }, 3000);
+}
 
-  constructor() { }
+onCreateServer() {
+    this.serverCreationStatus = 'Server is created';
+}
 
-  ngOnInit() {
-  }
+onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = event.target.value;
+}
+
+ngOnInit() {}
 
 }
